254dbe6e4cc2594f16c2f8ab3263d9696e15ec1e
Harshit Upreti <harshitupreti107@gmail.com>
2025-08-04 15:32:41 +0530

    Update and rename Assignment to Assignment.js
    added more if statement

---
+++ if-else/Assignment.js
@@ -0,0 +1,31 @@
+const username = prompt('Please enter your name.') || 'harsh'
+const age = +(prompt('Please enter your age.')) || 20
+console.log(`Name: ${username}`);
+console.log(`Age: ${age}`);
+
+if (userAge >= 0 && userAge <= 4) {
+    console.log(`${username} is a kid.`);
+    console.log('And he/she is playing.');
+}
+
+if (userAge >= 5 && userAge <= 17) {
+    console.log(`${username} is a school student.`);
+    console.log('And he/she is learning science and maths.');
+}
+
+if (userAge >= 18 && userAge <= 24) {
+    console.log(`${username} is a college student.`);
+    console.log('And he/she is learning computer science.');
+}
+
+if (userAge >= 25 && userAge <= 45) {
+    console.log(`${username} is a working professional.`);
+    console.log('And he/she is a web developer.');
+}
+
+if (userAge > 45) {
+    console.log(`${username} is retired.`);
+    console.log('And he/she reads newspaper.');
+}
+
+console.log('code ended');
