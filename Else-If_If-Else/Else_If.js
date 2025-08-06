8e29ee9f086e649a4936841771b8e03b95ff5808
Harshit Upreti <harshitupreti107@gmail.com>
2025-08-06 13:19:19 +0530

    Update Else_If.js

--- Else-If_If-Else/Else_If.js
+++ Else-If_If-Else/Else_If.js
@@ -8,25 +8,27 @@ if (userAge >= 0 && userAge <= 4) {
     console.log('And he/she is playing.');
 }
 
-if (userAge >= 5 && userAge <= 17) {
+else if (userAge >= 5 && userAge <= 17) {
     console.log(`${username} is a school student.`);
     console.log('And he/she is learning science and maths.');
 }
 
-if (userAge >= 18 && userAge <= 24) {
+else if (userAge >= 18 && userAge <= 24) {
     console.log(`${username} is a college student.`);
     console.log('And he/she is learning computer science.');
 }
 
-if (userAge >= 25 && userAge <= 45) {
+else if (userAge >= 25 && userAge <= 45) {
     console.log(`${username} is a working professional.`);
     console.log('And he/she is a web developer.');
 }
 
-if (userAge > 45) {
+else if (userAge > 45) {
     console.log(`${username} is retired.`);
     console.log('And he/she reads newspaper.');
 }
-
+else {
+    console.log('please enter a valid age');
+}    
 console.log('code ended');
 
