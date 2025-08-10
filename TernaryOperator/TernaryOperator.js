c2601f13ae04e0982faf2f7ff7a94b4c74646f48
Harshit Upreti <harshitupreti107@gmail.com>
2025-08-10 20:48:05 +0530

    Create TernaryOperator.js

---
+++ TernaryOperator/TernaryOperator.js
@@ -0,0 +1,8 @@
+// A simple example using a boolean condition
+let isRaining = true;
+
+// The ternary operator checks if 'isRaining' is true.
+// If it is, the value 'Take an umbrella.' is assigned to the 'weatherMessage' variable.
+// If it is false, the value 'Leave your umbrella at home.' is assigned instead.
+let weatherMessage = isRaining ? 'Take an umbrella.' : 'Leave your umbrella at home.';
+console.log('weatherMessage');
