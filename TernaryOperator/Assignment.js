c1e38adbaefb6aaf6e61be2a665bb1fe8b673c40
Harshit Upreti <harshitupreti107@gmail.com>
2025-08-19 20:13:10 +0530

    Delete TernaryOperator directory

--- TernaryOperator/Assignment.js
+++
@@ -1,7 +0,0 @@
-// Chaining ternary operators lets us to handle more than two outcomes in a single line, similar to an if-else if-else statement.
-// However, it's generally not recommended because it can make our code harder to read and debug.
-
-const age = 0 ? 'harshit' : '' ? 'hi' : "hello"
-console.log(age);
-// Output = "hello"
-// As 0 is a falsy value it will print empty string '', in further chain as '' is also a falsy value it will print "hello" in final output
