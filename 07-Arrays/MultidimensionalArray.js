d1e5622c84974894897fac52f97fd8dd0a717b3f
Harshit Upreti <harshitupreti107@gmail.com>
2025-08-21 23:20:06 +0530

    Create MultidimensionalArray.js

---
+++ 07-Arrays/MultidimensionalArray.js
@@ -0,0 +1,13 @@
+// --- 1. Creating a Multidimensional Array ---
+
+// A multidimensional array is an array that contains other arrays as its elements.
+// In this example, we create a 3x3 array to represent a Tic-Tac-Toe board.
+// Each inner array represents a row on the board.
+const ticTacToeBoard = [
+  ['X', 'O', 'X'], // This is the first row (index 0)
+  ['O', 'X', 'O'], // This is the second row (index 1)
+  ['O', 'O', 'X']  // This is the third row (index 2)
+];
+
+console.log('Original Board:');
+console.log(ticTacToeBoard);
