a00faa46de29f8d19113a29c010d67add666314e
Harshit Upreti <harshitupreti107@gmail.com>
2026-03-31 12:00:00 +0530

    recovering

--- 10-Functions In JS/Functions.js
+++ 10-Functions In JS/Functions.js
@@ -1,33 +1,16 @@
-// This is a function definition. 
-// The `function` keyword is used, followed by the function's name and parentheses.
-// The code to be executed is placed inside the curly braces `{}`.
-function introduceMe() {
-    console.log("Hey");
-    console.log("My name is Harshit Upreti");
-    console.log("I am a Btech Student");
+// function definition
+function IntroduceMe(username) {
+  console.log("Hello!");
+  console.log(`My name is ${username}`);
+  console.log("I am a student learning JavaScript");
 }
 
-// To use or "call" the function, you write its name followed by parentheses `()`.
-introduceMe();
+// function call
+// const returnvalue = IntroduceMe();
+IntroduceMe('harshit');
+console.log("**********************");
+IntroduceMe('john');
+console.log("**********************");
+IntroduceMe('jane');
+console.log("**********************");
 
-// Here, `username` and `profession` are parameters.
-function introduceMe(username, profession) {
-    console.log(`Hi, my name is ${username}.`);
-    console.log(`I am a ${profession}.`);
-}
-
-// When the function is called, the values "Anurag" and "Software Developer" 
-// are passed as arguments for the `username` and `profession` parameters, respectively.
-introduceMe("Harshit", "Btech student");
-introduceMe("Dibyanshu", "Biology student");
-
-// The return statement allows a function to produce and send back a value.
-// This returned value can then be used in other parts of your code.
-// This function adds two numbers and returns the result.
-function add(a, b) {
-    return a + b;
-}
-
-// The returned value (8) is stored in the `sum` variable.
-let sum = add(5, 3);
-console.log(sum); // Output: 8
