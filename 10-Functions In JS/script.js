fc027b5845062a4aa4d7ded606640545d94199c0
Harshit Upreti <harshitupreti107@gmail.com>
2026-04-18 07:43:34 +0530

    new day

---
+++ 10-Functions In JS/script.js
@@ -0,0 +1,14 @@
+function addtwonumber(a, b){
+    // debugger
+    // console.log(4+5);
+    return a+b;
+}
+
+// const result = addtwonumber(4,5);
+
+// function subtwonumber(a, b){
+//     return a-b;
+// }
+
+// const result2 = subtwonumber(6,5);
+// console.log(result2);
