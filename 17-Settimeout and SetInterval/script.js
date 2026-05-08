74a8e3db2cf6c54fa1ff253a22fc882e9ce23e88
Harshit Upreti <harshitupreti107@gmail.com>
2026-05-08 08:00:21 +0530

    new day

--- 17-Settimeout and SetInterval/script.js
+++ 17-Settimeout and SetInterval/script.js
@@ -1,2 +1,22 @@
-setTimeout(`console.log("Hi")`, 500)
-// first parameter is code part whereas second is time which tells in how much time our code will execute
+ // first parameter is code part whereas second is time which tells in how much time our code will execute
+// const timer1 = setTimeout(`console.log("Hi-1")`, 1000)
+// const timer2 = setTimeout(`console.log("Hi-2")`)
+// const timer3 = setTimeout(a, 3000, 'ssjd', 83293, [1,2,3])
+// const timer1 = setInterval(`console.log("Hi-1")`, 1000)
+// const timer2 = setTimeout(`console.log("Hi-2")`)
+const timer3 = setInterval(a, 3000, 'ssjd', 83293, [1,2,3])
+// clearTimeout(timer3)
+
+function a(){
+    console.log(arguments);
+    console.log("hiii");
+}
+
+// console.log('dhdsjbaejwbd');
+
+setTimeout(function() {
+    // debugger
+    console.log("hiii");  //asynchronous code
+})
+
+console.log("hi-2"); //synchronous code
