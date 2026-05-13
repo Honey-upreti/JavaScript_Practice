79b655cf29934f7d0866b3dc7b4393d2398b83bc
Harshit Upreti <harshitupreti107@gmail.com>
2026-05-13 07:49:45 +0530

    new day

---
+++ 20-Function vs Methods/script.js
@@ -0,0 +1,20 @@
+const math = {
+    E: 2.718281828459045,
+
+    add: function add(a, b) {
+        return a + b
+    },
+     sub(a, b) {
+        return a - b
+    },
+    square: function sqr(a) {
+        return a*a
+    },
+    cube(a){
+        return a**3
+    }
+}
+
+// These all are methods. When a function is put inside an object that function is called Methods
+// Every method is a function but every functions are not a method
+
