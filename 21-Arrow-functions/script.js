5f2ad9421bc45a1f5f66c658953cb5489cf654ea
Harshit Upreti <harshitupreti107@gmail.com>
2026-05-16 08:16:43 +0530

    new day

---
+++ 21-Arrow-functions/script.js
@@ -0,0 +1,30 @@
+// Function declaration
+
+// function square(num) {
+//     return num*num
+// }
+
+// function expression 
+
+// const square = function(num) {
+//     return num*num
+// }
+
+// Arrow function
+
+// const square = (num) => {
+//     return num * num
+// }
+
+
+const square = num =>
+    num * num
+
+const add = (num1, num2) =>
+    num1 + num2
+
+const random = () => Math.random()
+
+// setTimeout(()=>{
+//     console.log("hi");
+// },3000)
