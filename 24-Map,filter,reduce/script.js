5b5cc5c81ae360007f0df869adb91e0d6b03d5cd
Harshit Upreti <harshitupreti107@gmail.com>
2026-05-27 08:54:02 +0530

    new day

---
+++ 24-Map,filter,reduce/script.js
@@ -0,0 +1,51 @@
+const months = ["january", 'february', "March", "april", "May", 'december']
+
+// const capital= months.forEach((month, index)=>{
+//     console.log(index+1, month)
+//     return month.toUpperCase()
+// })
+
+const capital = months.map((month, index, array) => {
+    //     // console.log(index+1, month)
+    //     // console.log(array)
+    return month.toUpperCase()
+})
+
+// const filteredMonth= months.filter((month, index, array)=>{
+//     console.log(month)
+//     // console.log(array)
+//     return month.toLocaleUpperCase().includes('M')
+// })
+
+const filteredMonth = months.filter((month, index, array) => {
+    console.log(month)
+    // console.log(array)
+    return index >= 3
+})
+
+const students = [
+    {
+        Name: 'Harsh',
+        age: 21,
+    },
+    {
+        Name: 'Ankit',
+        age: 17,
+    },
+    {
+        Name: 'Gaurav',
+        age: 19,
+    },
+    {
+        Name: 'Amir',
+        age: 15,
+    },
+    {
+        Name: 'Abhi',
+        age: 13,
+    },
+]
+
+const adult = students.filter((student)=>{
+    return student.age>=18
+})
