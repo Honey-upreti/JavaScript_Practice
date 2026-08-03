2ab26242c07d7c5e12795e7c216a80c592c5270b
Harshit Upreti <harshitupreti107@gmail.com>
2026-08-03 08:34:33 +0530

    started DOM today and learned about manipulation of ddata, heirarchy in document

---
+++ 32-Document_Object_Model/script.js
@@ -0,0 +1,7 @@
+document.body.children[4].src = "https://i.pinimg.com/236x/7f/7a/c4/7f7ac41669175a7cfd85db609d5bce13.jpg"
+
+// Html parser in layman lang, is an engine in our browser which understands html element line by line and converts it into correspondance object of it
+
+// Parents and children in DOM , for eg body isa parent and h1,h2 are children. We get to know heirarchy from this concept
+// Change the origin of an element is know as DOM manipulation . For eg we can change text using innertext, innerHTml. 
+// In upper code we changeed src image 
