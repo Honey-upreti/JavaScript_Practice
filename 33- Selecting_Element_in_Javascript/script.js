c08783eaca45a856f90712175f082537d752589b
Harshit Upreti <harshitupreti107@gmail.com>
2026-08-04 10:08:16 +0530

    selecting element with getelements method in document

---
+++ 33- Selecting_Element_in_Javascript/script.js
@@ -0,0 +1,4 @@
+// const allImages = document.images
+const allImages = document.getElementsByTagName('img')
+// const cssImg = document.getElementsByClassName('css-img')[0]
+const cssImg = document.getElementById('css-img')
