33519fbc4aaeea9f1c4f19f192c222fa87538576
Harshit Upreti <harshitupreti107@gmail.com>
2026-08-16 20:33:09 +0530

    learned about node in js, basically every element is a node and we can see that in it's properties

---
+++ 38-Node vs Elements/script.js
@@ -0,0 +1,5 @@
+// document.body.childNodes
+// this will return an array of it's child elements.
+// Which means every element is a node in itself but every node is not an element 
+
+// Every element has different nodevalue such as Element Node has 1 value 
