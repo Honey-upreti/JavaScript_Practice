11221f6f45ea44ae6dd506e0d9f8226ae87d95ef
Harshit Upreti <harshitupreti107@gmail.com>
2026-08-22 08:43:27 +0530

    tried addeventlistener() method, and a small task of creating more card on first card click

--- 42-Events in JavaScript/script.js
+++ 42-Events in JavaScript/script.js
@@ -3,5 +3,25 @@ const h1 = document.querySelector('h1')
 function sayHi (){
     console.log('hiiiii')
 }
+function secondhi(){
+    console.log("2nd hi")
+}
 
-h1.onclick = sayHi
+// h1.onclick = sayHi
+// h1.onclick = secondhi
+
+// Most used method 
+// h1.addEventListener("click", sayHi)
+// h1.addEventListener("click", function(){
+//     console.log("2nd hi")
+// })
+const card = document.querySelector('.card')
+const container = document.querySelector(".container")
+function addcard(){
+    const newcard = document.createElement('div')
+    newcard.classList.add('card')
+    container.appendChild(newcard)
+}
+
+
+card.addEventListener("click",addcard)
