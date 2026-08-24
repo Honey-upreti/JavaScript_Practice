4fab329910f1126a768c79c36a04d90e399042bb
Harshit Upreti <harshitupreti107@gmail.com>
2026-08-24 08:22:21 +0530

    adding events in js, tried how clicking in'+' would append new cards with number 1,2,3...

--- 42-Events in JavaScript/script.js
+++ 42-Events in JavaScript/script.js
@@ -1,9 +1,9 @@
 const h1 = document.querySelector('h1')
 
-function sayHi (){
+function sayHi() {
     console.log('hiiiii')
 }
-function secondhi(){
+function secondhi() {
     console.log("2nd hi")
 }
 
@@ -15,13 +15,18 @@ function secondhi(){
 // h1.addEventListener("click", function(){
 //     console.log("2nd hi")
 // })
+
+let count = 0
 const card = document.querySelector('.card')
 const container = document.querySelector(".container")
-function addcard(){
+function addcard() {
     const newcard = document.createElement('div')
     newcard.classList.add('card')
+    // const newcard = card.cloneNode()
+    // newcard.classList.remove('add-card')  //cloneNode copies whole classes of an element hence we have to remove that class which we don't need
+    newcard.innerText = count
+    count++
     container.appendChild(newcard)
 }
+card.addEventListener("click", addcard)
 
-
-card.addEventListener("click",addcard)
