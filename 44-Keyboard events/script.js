8734e9c6e512752b2eb9e2882c9b07694d466be2
Harshit Upreti <harshitupreti107@gmail.com>
2026-08-31 08:08:47 +0530

    keyboard events such as keypress, keyup, keydown

--- 44-Keyboard events/script.js
+++ 44-Keyboard events/script.js
@@ -1,5 +1,16 @@
-const h1 = document.querySelector('h1')
+// const h1 = document.querySelector('h1')
 // const input = document.querySelector('input')
-h1 .addEventListener('keypress', (e) => {
-    console.log(e.key);
+window.addEventListener('keypress', (a) => {
+    console.log('key code:',a.code);
+    console.log('key pressed:',a.key);
 })
+
+// window.addEventListener('keyup', (e) => {
+//     console.log('key code:',e.code);
+//     console.log('key pressed:',e.key);
+// })
+
+// window.addEventListener('keydown', (e) => {
+//     console.log('key code:',e.code);
+//     console.log('key pressed:',e.key);
+// })
