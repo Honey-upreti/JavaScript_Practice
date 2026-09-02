89959f073715d929c92f430f3eb03026c8745870
Harshit Upreti <harshitupreti107@gmail.com>
2026-09-02 07:57:24 +0530

    mouse events such as click, mousedown, mouseup and touch events for mobile and finally pointer event for both mouse and touch

---
+++ 45-Mouse events/script.js
@@ -0,0 +1,109 @@
+const h1 = document.querySelector('h1')
+const addcardButton = document.querySelector('.card')
+const container = document.querySelector('.container')
+
+let count = 0
+
+// addcardButton.addEventListener('click', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// addcardButton.addEventListener('mousedown', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// addcardButton.addEventListener('mouseup', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// addcardButton.addEventListener('mouseenter', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// addcardButton.addEventListener('mouseleave', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// addcardButton.addEventListener('mousemove', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// addcardButton.addEventListener('mouseover', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// document.addEventListener('wheel', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// }
+
+// document.addEventListener('scroll', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// addcardButton.addEventListener('touchstart', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// addcardButton.addEventListener('touchend', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// addcardButton.addEventListener('touchmove', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+addcardButton.addEventListener('pointermove', (e) => {
+    console.log(e);
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+    newCard.innerText = count++
+    container.append(newCard)
+})
