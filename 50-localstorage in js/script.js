5fdd7ab52b126ff3c6d53a8fc02b10781a82ec09
Harshit Upreti <harshitupreti107@gmail.com>
2026-09-12 21:13:31 +0530

    new method to store key value in local storage, by using object then stringify it

--- 50-localstorage in js/script.js
+++ 50-localstorage in js/script.js
@@ -1,21 +1,41 @@
 const nameElement = document.querySelector('.name-tag')
 const nameInput = document.querySelector('.name')
-nameElement.innerText = localStorage.getItem('myName')  // this is when we reload, to hold the text
+nameElement.innerText = localStorage.getItem('myName')  
+// this is when we reload, to hold the text
 const ageInput = document.querySelector('.age')
 const ageElement = document.querySelector('.age-tag')
 
-nameInput.addEventListener('input', (e) => {
-    // localStorage.myName = e.target.value;
-    localStorage.setItem('myName', e.target.value)
-    nameElement.innerText = localStorage.getItem('myName')  //this is when we type anything in input we see the result as text
+// nameInput.addEventListener('input', (e) => {
+//     // localStorage.myName = e.target.value;
+//     localStorage.setItem('myName', e.target.value)
+//     nameElement.innerText = localStorage.getItem('myName')
+//   //this is when we type anything in input we see the result as text
+
+// })
+
+// ageElement.innerText = localStorage.getItem('myage')
+
+// ageInput.addEventListener('input', (e) => {
+
+//     localStorage.setItem('myage', e.target.value)
+//     ageElement.innerText = localStorage.getItem('myage')
+
+// })
+
+const mydata = JSON.parse(localStorage.getItem('mydata'))  || {}
+
+nameInput.addEventListener('input',(e)=>{
+    mydata.name = e.target.value
+    localStorage.setItem('mydata', JSON.stringify(mydata))
+    nameElement.innerText = e.target.value
 
 })
 
-ageElement.innerText = localStorage.getItem('myage')
-
-ageInput.addEventListener('input', (e) => {
-
-    localStorage.setItem('myage', e.target.value)
-    ageElement.innerText = localStorage.getItem('myage')
-
+ageInput.addEventListener('input',(e)=>{
+     mydata.age = e.target.value
+    localStorage.setItem('mydata', JSON.stringify(mydata))
+    ageElement.innerText = e.target.value
 })
+
+ageElement.innerHTML = mydata.age
+nameElement.innerText = mydata.name
